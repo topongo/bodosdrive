b423e6fb512a15597f669e3d7412222ac6f6912b
Lorenzo Bodini <lorenzo.bodini.private@gmail.com>
2021-09-27 22:31:26 +0000

    Updated TopongoConfigs, still testing stage for actual main file, changed templates MOD changed templates to actually work MOD testing stuff on bodosdrive.py

--- bodosdrive.py
+++ bodosdrive.py
@@ -1,12 +1,17 @@
-import os
+import os.path
 import json
 from TopongoConfigs.configs import Configs
-from templates import config_types as __config_types
+from templates import config_defaults as __config_defaults
 
-CONFIG_PATH = os.path.expanduser('~/.config/bodosdrive/config.json')
+CONFIG_PATH = os.path.expandvars('$HOME/.config/bodosdrive/config.json')
 
 if not os.path.exists(CONFIG_PATH):
-    from templates import config_defaults as __config_defaults
-    conf = Configs(__config_types, __config_defaults)
+    conf = Configs(__config_defaults)
+    if not os.path.exists(os.path.dirname(CONFIG_PATH)):
+        os.makedirs(os.path.dirname(CONFIG_PATH))
+    conf.write(open(CONFIG_PATH, "w+"))
+    print("Created config file with default values:")
+    conf.write()
 else:
-    conf = Configs(__config_types, json.load(open(CONFIG_PATH)))
+    conf = Configs(__config_defaults, config_path=open(CONFIG_PATH))
+    conf.write()
--- templates.py
+++ templates.py
@@ -1,24 +1,4 @@
-config_types = {
-    "version": float,
-    "test": {
-        "x": int,
-        "y": int,
-        "z": {
-            0: str,
-            2: str
-        }
-    }
-
-}
-
 config_defaults = {
     "version": 0.1,
-    "test": {
-        "x": 10,
-        "y": 20,
-        "z": {
-            0: "zoz",
-            2: "zez"
-        }
-    }
+    "config_file": "$USER/.config/bodosdrive/config.json"
 }
